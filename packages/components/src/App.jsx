b2470e754ac71bf6d36cccad9000f8320e753652
Shad <shadrais321@outlook.com>
2022-07-14 18:32:34 +0530

    Test 3

--- packages/components/src/App.jsx
+++ packages/components/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Components test 2</p>
+        <p>Components test 3</p>
         <p>
           <button type='button' onClick={() => setCount((count) => count + 1)}>
             count is: {count}
